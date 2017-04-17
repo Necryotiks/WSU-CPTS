2cd319c1d1053ab461f667d845ece17f51fd37bd
Necryotiks <eman13vil@gmail.com>
2017-04-17 14:17:52 -0700

    moved files

--- Attendence Tracker/Attendence Tracker/main.cpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "Listnode.h"
-
-int main()
-{
-	run_exe();
-}
--- Data Analysis/Data Analysis/main.cpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "DataAnalysis.h"
-
-int main()
- {
-	DataAnalysis OBJ;
-	OBJ.runAnalysis();
- }
